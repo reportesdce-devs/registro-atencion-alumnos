2219362506a92a09f7382bcf7bb42ad560531f76
reportesdce-devs <reportesdce@iest.edu.mx>
2026-07-13 17:32:35 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbw39mYacVCZN2dGhflLHQClmDSGP-zKUD_5FkQ9VqO1Bca105cMh6cGCMFji0xSR0V7/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbz0masSVS5oREAQYUJxmxy-8mo_AXs4Naewi44I550V7V64wackGZ7F3hryI6Cvv3Hq8A/exec"
 };
