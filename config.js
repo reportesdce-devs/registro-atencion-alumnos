1ec285b21976cc4e1c1964054fa86cab08341c15
reportesdce-devs <reportesdce@iest.edu.mx>
2026-07-13 16:54:56 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/a/macros/iest.edu.mx/s/AKfycbw39mYacVCZN2dGhflLHQClmDSGP-zKUD_5FkQ9VqO1Bca105cMh6cGCMFji0xSR0V7/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbw39mYacVCZN2dGhflLHQClmDSGP-zKUD_5FkQ9VqO1Bca105cMh6cGCMFji0xSR0V7/exec"
 };
